cb492755ef85ae08be1bae5452b429d873a31268
shashi <shashipolity@gmail.com>
2022-12-11 16:15:23 +0530

    bulk uploaded

--- main.py
+++ main.py
@@ -22,15 +22,13 @@ if __name__=="__main__":
           
           #data validation
           data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
-          data_validation = DataValidation(data_validation_config=data_validation_config,
-                         data_ingestion_artifact=data_ingestion_artifact)
+          data_validation = DataValidation(data_validation_config=data_validation_config,data_ingestion_artifact=data_ingestion_artifact)
 
           data_validation_artifact = data_validation.initiate_data_validation()
 
           #data transformation
           data_transformation_config = config_entity.DataTransformationConfig(training_pipeline_config=training_pipeline_config)
-          data_transformation = DataTransformation(data_transformation_config=data_transformation_config, 
-          data_ingestion_artifact=data_ingestion_artifact)
+          data_transformation = DataTransformation(data_transformation_config=data_transformation_config, data_ingestion_artifact=data_ingestion_artifact)
           data_transformation_artifact = data_transformation.initiate_data_transformation()
           
           #model trainer
@@ -39,10 +37,7 @@ if __name__=="__main__":
           model_trainer_artifact = model_trainer.initiate_model_trainer()
 
           model_eval_config = config_entity.ModelEvaluationConfig(training_pipeline_config=training_pipeline_config)
-          model_eval  = ModelEvaluation(model_eval_config=model_eval_config,
-           data_ingestion_artifact=data_ingestion_artifact,
-           data_transformation_artifact=data_transformation_artifact,
-            model_trainer_artifact=model_trainer_artifact)
+          model_eval  = ModelEvaluation(model_eval_config=model_eval_config,data_ingestion_artifact=data_ingestion_artifact,data_transformation_artifact=data_transformation_artifact,model_trainer_artifact=model_trainer_artifact)
           model_eval_artifact = model_eval.initiate_model_evaluation()
           
      except Exception as e:
